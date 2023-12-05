b079248570aca72091b7a1851993a53414a743c5
jackyayUNC <jackyay@email.unc.edu>
2023-12-05 18:40:13 -0500

    roster as loop

--- roster.py
+++ roster.py
@@ -1,5 +1,21 @@
 #https://goheels.com/sports/mens-basketball/roster
 
-roster = ["Bactor", "Davis", "Cadeau"]
-for player in roster:
-    print(player)
+import pandas as pd
+
+player = {"Last Name": ("High", "Cadeau", "Ryan", "Davis", "Bacot",
+                        "Trimble", "Wojcik", "Washington", "Lebo", "Landry"),
+          "First Name": ("Zayden", "Elliot", "Cormac",
+                         "RJ", "Armando", "Seth",
+                         "Paxson", "Jalen", "Creighton", "Rob"),
+        "height": [81, 72, 77, 72, 83, 75, 77, 82, 73, 76],
+        "weight": [225, 180, 195, 180, 240, 195, 195, 230, 180, 190]
+        }
+data = pd.DataFrame(player)
+
+
+data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
+data["bmi"] = data["bmi"].round(2)
+
+print(data)
+data.to_csv("bmi.csv")
+
